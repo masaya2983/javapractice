af2c37089964e808e3db3128bd2da0f9db016c67
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-01 14:25:54 +0000

    コメント

---
+++ 1/1-4.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main (string[] args) {
+      double pie  = 3.14;
+      int pie  = 5;
+      System.out.println("半径" +  pie  + "cmのパイの面積");
+      System.out.println("pie * pie * pie");
+      System.out.println("パイの半径を倍にします")
+      pie = 10;
+      System.out.println("半径" + pie + "cmのパイ"
+      System.out.println(pie  * pie * pie)
+    }
+    }
