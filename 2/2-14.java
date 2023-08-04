eaeb203c529dafc4295cb6f7138f26f947d75a84
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-04 12:45:21 +0000

    コメント

--- 2/2-14.java
+++ 2/2-14.java
@@ -1,3 +1,6 @@
 public class Main{
-  public static void main (string[] args)
+  public static void main (string[] args){
+    int r = new java.unil.Random().nextInt(90);
+    System.out.println("あなたはたぶん" + r +"歳ですね");
+  }
 }
