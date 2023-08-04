85ba4ff8fe86514675ea20601c9d1307e3494e35
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-04 12:29:41 +0000

    コメント

--- 2/2-13.java
+++ 2/2-13.java
@@ -1,3 +1,7 @@
 public class Main{
-  public static void main (string[] args)
+  public static void main (string[] args){
+    String  age = "31";
+    int n Integer parseInt(age);
+    System.print.outln("あなたは来年、" + (n  + 1)+ "歳になりますね");
+  }
 }
