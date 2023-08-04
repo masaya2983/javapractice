248e431c155c4946e0f14ffe4bd815f6259d9616
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-04 12:13:02 +0000

    コメント

---
+++ 2/2-11.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main (string[] args){
+    String  name  = "すがわら";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.prinht("です");
+  }
+}
