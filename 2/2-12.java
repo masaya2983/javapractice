f4b7487fec744b03b8037bbcef00d5a402db457a
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-04 12:20:48 +0000

    コメント

--- 2/2-12.java
+++ 2/2-12.java
@@ -1,5 +1,8 @@
 public class Main{
   public static void main (string[] args){
-    
+    int a = 5;
+    int b = 3;
+    int m  = Math.max(a.b);
+    System.out.print("比較実験:"  + a +"とで大きいほうは・・・" + m);
   }
 }
