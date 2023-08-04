4403fa302a0fd51643081eb9a2f9d11e3f178fac
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-04 13:03:46 +0000

    コメント

--- 2/2-15.java
+++ 2/2-15.java
@@ -1,5 +1,9 @@
 public class Main{
   public static void main (string[] args){
-    
+    System.out.println("あなたの名前を入力してください");
+    String  name  = new java.util.Scanner(System.in).nextLine();
+    System.out.println("あなたの年齢を入力してください。");
+    int age = new java.util.Scanner(System.in).NextInt();
+    System.out.println("ようこそ" + age + "歳の"  + name  + "さん");
   }
 }
