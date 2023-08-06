4675c8bfc240b72bc3754d1969454e04d82a78ab
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-06 11:57:24 +0000

    コメント

---
+++ 3/3-2.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main (string[] args){
+    boolean doorClose = true;
+    while (doorClose ==  true){
+      System.out.println("ノックする");
+      System.out.println("1分待つ");
+    }
+  }
+}
