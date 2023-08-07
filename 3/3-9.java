7372664b6e81f929abc804e2deba39c87e298130
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-07 14:13:42 +0000

    コメント

--- 3/3-9.java
+++ 3/3-9.java
@@ -1,5 +1,11 @@
 public class Main{
    public static void main (string[] args){
-     
+     for (int i = i;  i < 10; i++ ){
+       for (int j = i;  j < 10 ;j++ ){
+         System.out.println(i * j);
+         System.out.println("");
+       }
+       System.out.println("");
+     }
    }
 }
