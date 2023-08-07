bca50181a1b9605c45f1390b001a1bb0c8158204
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-07 12:46:26 +0000

    コメント

---
+++ 3/3-6.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main (string[] args){
+    System.out.println("あなたの運勢を占います");
+    int fortune = new java.util.Random().nextInt(5) + 1;
+    switch(foryune){
+      case 1:
+      case 2:
+        System.out.println("いいね");
+        break;
+      case 3:
+        System.out.println("普通です");
+        break;
+      case 4:
+      case 5:
+        System.out.println("うーん・・・");
+    }
+  }
+}
