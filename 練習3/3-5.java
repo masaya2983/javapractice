c8099de75248778e9f19b00be10774eea636b5a1
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-08 13:42:46 +0000

    コメント

---
+++ 練習3/3-5.java
@@ -0,0 +1,23 @@
+public class Main{
+  public static void main (string[] args){
+    System.out.print("[メニュー]  1:検索  2:登録  3:削除  4変更");
+
+    int serected  = new java.util.Scanner(System.in).nextInt();
+    switch  (selected){
+      case 1:
+        System.out.plintln("検索します");
+        break
+      case 2:
+        System.out.println("登録します");
+        break
+      case 3:
+        System.out.println("削除します");
+        break
+      case 4:
+
+      System.out.println("変更します");
+      break
+
+    }
+  }
+}
