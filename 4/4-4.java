b29419b20c6619001da242277a18212f168a6383
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-09 13:48:19 +0000

    配列

--- 4/4-4.java
+++ 4/4-4.java
@@ -2,6 +2,6 @@ public class Main{
   public static void main (string[] args){
     int []  scores  = new int[5];
     int num = scores.lenght;
-    System.out.println("要素の数:"  +num);
+    System.out.println("要素の数:"  + num);
   }
 }
--- 4/4-5.java
+++ 4/4-5.java
@@ -1,5 +1,8 @@
 public class Main{
   public static void main (string[] args){
-    
+    int []  scores;
+    scores  = new int [5];
+    scores  [1] = 30;
+    System.out.println(scores[1])
   }
 }
