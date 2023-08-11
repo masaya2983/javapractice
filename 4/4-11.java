b53d742f6baee8eb147c18dc0631b411ae0d5237
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-11 12:22:19 +0000

    java

--- 4/4-11.java
+++ 4/4-11.java
@@ -1,6 +1,10 @@
 public class Main{
   public static void main (string[] args){
-    
-    
+    int [] scores = {20, 30, 40, 50, 80};
+    int count = 0;
+    for (int i = 0; i<scores[i] >=50){
+      count++;
+    }
+    System.out.println("50点以上の科目の数は:" +count);
   }
 }
