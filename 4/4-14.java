64665da2962200be1761886ee02d2ed569c8ae3c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-12 05:22:23 +0000

    java

--- 4/4-14.java
+++ 4/4-14.java
@@ -1,6 +1,9 @@
 public class Main{
   public static void main (string[] args){
-    
+    int [] scores = {20, 30, 40, 80};
+    for (int value :scores) {
+      System.out.println(value);
+    }
     
   }
 }
