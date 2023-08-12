e66faad897f6590dea6bda7ca6adad6334c2a277
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-12 05:11:37 +0000

    java

--- 4/4-12.java
+++ 4/4-12.java
@@ -1,6 +1,24 @@
 public class Main{
   public static void main (string[] args){
-    
-    
+    int [] seq = new int [10];
+    for (int i = 0 i < seq.length; i++){
+      seq(i) = new java.util.Rabdom().nextInt(4);
+    }
+    for (int i = 0; i < seq.length; i++){
+      switch{
+      case 0:
+        System.out.println("A");
+        break
+      case 1:
+        System.out.println("T");
+        break
+      case 2:
+        System.out.println("G");
+        break
+      case 3:
+        System.out.println("C");
+        break
+    }
+    }
   }
 }
