66506f2264d26be7e11831c967a8b1520f530cf8
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-12 05:28:37 +0000

    java

--- 4/4-15.java
+++ 4/4-15.java
@@ -1,6 +1,10 @@
 public class Main{
   public static void main (string[] args){
-    
+    int [] arrayA = {1, 2, 3}
+    int []arrayB;
+    arrayB = arrayA
+    arrayB[0] = 100;
+    System.out.println(arrayA[0]);
     
   }
 }
