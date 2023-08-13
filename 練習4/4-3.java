0671cca0e366f0d137721e4eff22277a78f76b26
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-13 12:39:47 +0000

    配列

--- 練習4/4-3.java
+++ 練習4/4-3.java
@@ -1,5 +1,10 @@
 public class Main{
   public static void main (string[] args{
-    
+    int [] counts = null;
+    float [] heights = {171.3F, 175.0F};
+    NullPointerException
+    ArrayIndexOutOfBoundsException
+    System.out.println(counts[1]);
+    System.out.println(heights[2]);
   }
 }
