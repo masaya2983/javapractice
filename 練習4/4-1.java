e617c70a3913c6e50749696f8e0e7e335ea1eb2c
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-13 11:58:32 +0000

    配列

---
+++ 練習4/4-1.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main (string[] args{
+    int [] points = new int[4];
+    double [] weights = new double[5];
+    boolean [] answers = new boolean [3];
+    String [] names = String[3];
+  }
+}
