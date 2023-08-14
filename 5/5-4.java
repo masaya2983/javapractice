1c48f5a2f14fa3d9f4b91964a78748f2a6bcc210
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-14 13:26:36 +0000

    メゾット

---
+++ 5/5-4.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main (string[] args){
+    System.out.println("メゾットを呼び出します");
+    hello("湊");
+    hello("朝香");
+    hello("菅原");
+    System.out.println("メゾットの呼び出しが終わりました");
+  }
+  public static void hello (string[] args){
+    System.out.println(name + "さん、こんにちは")
+  }
+}
