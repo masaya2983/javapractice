5ddfe7cb7526f62d1e85ccb1e493a532537f5df4
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-14 12:04:50 +0000

    メゾット

--- 5/5-1.java
+++ 5/5-1.java
@@ -1,5 +1,5 @@
 public class Main{
   public static void main (string[] args){
-    System.out.println("湊さんこんにちは");
+    System.out.println("湊さん、こんにちは");
   }
 }
---
+++ 5/5-2.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main (string[] args){
+    System.out.println("メゾットを呼びだします");
+    hello();
+    System.out.println("メゾットを呼びだしが終わりました");
+  }
+  public static void hello(){
+    System.out.println("湊さん、こんにちは");
+  }
+}
