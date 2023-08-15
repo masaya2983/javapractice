0faf5ae3b2ef43726712db9dbf7bffc92d560102
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-15 13:19:07 +0000

    メゾット

---
+++ 5/5-8.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static int add(int x, int y){
+    int ans = x + y;
+    return ans;
+    public static void add(){
+      System.out.println(add(101, 20), add(30, 40));
+    }
+  }
+}
