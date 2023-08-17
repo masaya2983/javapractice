a752166758a3677bb75806beb5f7bd130df8af91
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-17 14:31:25 +0000

    メゾット

---
+++ 5/5-14.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static int[] makeArray(int size){
+    int [] newArray = new int [size]{
+      int[] newArray;
+    }
+    return newArray;
+  }
+  public sta main(String[] args){
+    int[] array = makeArray(3);
+    incArray(array);
+    for (int i : array){
+      System.out.println(i);
+    }
+  }
+}
