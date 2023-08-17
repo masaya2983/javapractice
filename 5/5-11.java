436afa0447d5239a23504e32803d3d51e378d152
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-17 13:35:45 +0000

    メゾット

--- 5/5-11.java
+++ 5/5-11.java
@@ -1,5 +1,12 @@
 public class Main{
   public static int add(int x, int y){
-    
+    return x + y;
+  }
+  public static double add(int x, int y, int z){
+  return x + y + z;
+  }
+  public static void public static void main (String[] args) {
+    System.out.println("10+20=" +  add(10, 20));
+    System.out.println("10+20+30=" +  add(10, 20, 30));
   }
 }
