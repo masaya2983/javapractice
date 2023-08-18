fbe1fb80eca3a14b6f1dbcdecf7bdf55b788afcd
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-18 02:53:10 +0000

    メゾット

--- 練習5/5-1.java
+++ 練習5/5-1.java
@@ -10,6 +10,6 @@ public class Main{
     System.out.println("私の名前は" + name +"です");
     System.out.println("歳は" + age + "です");
     System.out.println("身長は" + height + "cmです");
-    System.out.println("十二支は" + zodiac + "")
+    System.out.println("十二支は" + zodiac + "です")
   }
 }
