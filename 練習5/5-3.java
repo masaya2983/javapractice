81b5706bab442b450a07dd93e737982a556aeb48
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-18 03:06:39 +0000

    メゾット

---
+++ 練習5/5-3.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main (string[] args{
+    String address = "us800@xxxx.com";
+    String text = "今度、飲みに行きませんか";
+    email (address, text);
+  }
+  public static void email(String address, String text){
+    System.out.println
+    (address + "に、以下のメールを送信しました");
+    System.out.println("件名 :無題");
+    System.out.println("本文 :" + text);
+  }
+  public static void email(String title, String address, String text){
+    System.out.println(address + "に、以下のメールを送信しました");
+     System.out.println("件名 :" + title);
+      System.out.println("本文 :" + text);
+  }
+}
