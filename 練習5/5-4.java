d56b1b5d243283e72d5ad69ca2d220ff638bb175
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-18 04:03:50 +0000

    メゾット

---
+++ 練習5/5-4.java
@@ -0,0 +1,17 @@
+public class Main{
+  public static void main (string[] args{
+    double triangleArea = calcTriangleArea(10.0, 5.0);
+    System.out.println("三角形の面積 :" + triangleArea + "平方cm");
+    double circleArea = calcCircleArea(5.0);
+    System.out.println("円の面積 :" + calcCircleArea  + "平方cm");
+  }
+  public static double calcTriangleArea(double bottom, double height) {
+    double area = (bottom * height) / 2;
+    return area;
+  }
+  public static double calcTriangleArea(double radius) {
+    double area = radius * radius * 3.14;
+    
+    return area;
+  }
+}
