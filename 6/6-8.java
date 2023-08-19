2ab5082ef3618de2897858b2b9bf48a8aab3750d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-19 03:18:54 +0000

    ソースファイルを分割

---
+++ 6/6-8.java
@@ -0,0 +1,9 @@
+package calcapp.main;
+import calcapp.logics.CalcLogic;
+
+public class Calc{
+  public static void main(String []args){
+    int total = CalaLogic.tasu(a,b);
+    int delta = calcapp.logics.hiku(a,b);
+  }
+}
