eebc4ee1b5626a00d88f59b683884b12826c3a6d
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-19 12:48:08 +0000

    ソースファイルを分割

---
+++ 練習６/Main.java
@@ -0,0 +1,11 @@
+import comment.Zenhan;
+
+public class Main{
+  public static void main (String[]arg) throws Exception{
+    Zenhan.doWarusa();
+    Zenhan.doTogame();
+    comment.Kouhan
+    comment.Kouhan.callDeae();
+    comment.kouhan.showMondokoro();
+  }
+}
