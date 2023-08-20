252bcc4fb39ad2f106ba77060fdfb6d6063bb7aa
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-20 13:45:24 +0000

    インスタンスとクラス

---
+++ 8/8-14.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args{
+    int yusha_hp = 100;
+    int matango1_hp = 50;
+    int matango2_hp = 48;
+    String yusha_name = "ミナト";
+    int matango1_level = 10;
+    int matango1_leve2 = 10;
+    
+    
+    System.out.println(yusha_name + "は、５秒座った!");
+    yusha_hp += 5;
+    System.out.println("HPが５ポイント回復した");
+  }
+}
