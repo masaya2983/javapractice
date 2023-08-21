9695bf9643af1655dd86f64e5b64c1244b41ab40
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-21 02:45:03 +0000

    インスタンスとクラス

--- 8/8-16.java
+++ 8/8-16.java
@@ -1,8 +1,11 @@
-public class Matango{
-  int hp;
-  final int LEVEL = 10;
-  char suffix;
-  public void run(){
-    System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+public class Main{
+  public static void main(String[]argss){
+    Hero h = new Hero():
+    h.name = "ミナト";
+    h.hp = 100;
+    
+    Matango m1 = new Matango();
+    m1.hp = 50;
+    m1.suffix = 'A';
   }
 }
