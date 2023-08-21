8646197dfa490913b03c8961b07fc16765890889
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-21 02:48:35 +0000

    インスタンスとクラス

--- 8/8-16.java
+++ 8/8-16.java
@@ -7,5 +7,15 @@ public class Main{
     Matango m1 = new Matango();
     m1.hp = 50;
     m1.suffix = 'A';
+    
+    Matango m2 = new Matango();
+    m1.hp = 48;
+    m1.suffix = 'B';
+    
+    //冒険のはじまり
+    h.slip ();
+    m1.run ();
+    m2 run ();
+    h.run ();
   }
 }
