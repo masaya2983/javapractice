5166fd8d359381de1a596238c72becd879fd93df
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-23 12:03:12 +0000

    継承

---
+++ 10/10-7.java
@@ -0,0 +1,12 @@
+public class Hero{
+  
+  public final void slip(){
+    this.hp -= 5;
+    System.out.println(this.name + "、は転んだ！");
+    System.out.println("5のダメージ");
+  }
+  public void run () {
+  System.out.println(this..name + "は逃げ出した!");  
+  }
+  
+}
