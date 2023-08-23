752304dad0f8664bde9ae9863c3c3c28a16c49bd
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-23 12:51:57 +0000

    継承

--- 10/10-10/Hero.java
+++ 10/10-10/Hero.java
@@ -3,4 +3,5 @@ public class Hero {
   public Hero(){
     System.out.println("Heroのコンストラクタが動作");
   }
+  
 }
---
+++ 10/10-10/SuperHero.java
@@ -0,0 +1,6 @@
+public class SuperHero extends Hero{
+  
+  public SuperHero(){
+    System.out.println("SuperHeroのコンストラクタが動作");
+  }
+}
