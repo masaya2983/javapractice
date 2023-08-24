97ba2c6947bfed7afa196c45324315c882adeafa
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-24 12:01:12 +0000

    継承

--- 11/Main.java
+++ 11/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main (String[] args) {
+    Character c = new Character();
     Hero h = new Hero();
-    Matango m = new Matango();
+    Matango m = new Matango('A');
     h.attack(m);
   }
 }
