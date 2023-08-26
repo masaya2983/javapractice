bb17eb881536d52b6642a9876b3c47a6a77405e5
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-26 02:20:50 +0000

    継承

--- 12/Main.java
+++ 12/Main.java
@@ -1,10 +1,11 @@
 public class Main{
   public static void main (String[] args){
     Wizard w = new Wizard();
+    Character c = w;
     Matango m = new Matango();
-    w.name = "アサカ";
-    w.attack(m);
-    w.fireball(m);
+    c.name = "アサカ";
+    c.attack(m);
+    c.fireball(m);
     Life if = new Wizard;
   }
 }
