ad2c93fa926b54b703058e12d73adebc8ea495da
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-27 02:29:17 +0000

    継承

---
+++ 12/12-9/Hero.java
@@ -0,0 +1,8 @@
+public class Hero extends Character {
+  public void attack(Moster m){
+    System.out.println(this.name + "の攻撃!");
+    System.out.println("敵に１０ポイントのダメージをあたえた");
+    m.hp -= 10
+  }
+  
+}
