6c3a1fb07023b692bafcd1795e3779aaa444203e
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-30 14:27:16 +0000

    カプセル

---
+++ 練習13/13-3/Wand.java
@@ -0,0 +1,10 @@
+public class Wand{
+  private String name;
+  private double power;
+  public String getName() {return this.name;}
+  public void setName() {this.name = name;}
+  public double getPower(){ return this.power;}
+  public void setPower(double power){this.power = power;
+    
+  
+}
