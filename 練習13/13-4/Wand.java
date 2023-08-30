0e3f0f7878c7228d76b09f17993c59a8d83031c4
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-08-30 14:51:57 +0000

    カプセル

---
+++ 練習13/13-4/Wand.java
@@ -0,0 +1,22 @@
+public class Wand{
+  private String name;
+  private double power;
+  public String getName() {return this.name;}
+  public void setName(String name) {
+ 
+ 
+   if(name == null  || name.length() <3) {
+     throw new IllegalArgumentException("杖に設定され用としている名前が異常です")；
+   }   
+  　this.name = name;
+  　
+}
+public double getPower() {return this.power;}
+public void setPower(double power){
+  if(power <0.5 ||poer >100.0){
+    throw new IllegalArgumentException("杖に設定されようとしている魔力が異常です");
+  }
+  this.power = power;
+}
+   
+  }
