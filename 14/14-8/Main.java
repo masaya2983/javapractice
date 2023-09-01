a20e150f3324bfd9ac6bb59b54d6ff4ab478532b
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-01 14:03:18 +0000

    Javaを支えるクラスたち

---
+++ 14/14-8/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+ public static void main(String[] args){
+   Hero h1 = new Hero();
+   Hero h2 = new Hero();
+   System.out.println(h1.hp);
+   System.out.primtln(Hero.money);
+ }
+}
