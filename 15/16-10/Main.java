4d49f4ff6b708f002b86c5ece3856ae96378cdc6
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-04 13:02:31 +0000

    Javaを支えるクラスたち

---
+++ 15/16-10/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+  public static void main(String[] args) {
+    long start = System.currentTimeMillis();
+    //ここで何らかの時間がかかる処理
+    long end = System.currentTimeMills();
+    System.out.println("処理にかかった時間は・・・" + (end - start) + "ミリ秒でした");
+  }
+}
