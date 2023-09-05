2fc9eb342af831e7c533f589fd29888061847838
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-05 06:15:41 +0000

    Javaを支えるクラスたち

--- 15/15-14/Main.java
+++ 15/15-14/Main.java
@@ -11,6 +11,16 @@ public class Main{
     
     //ZonedDateTimeの生成
     ZonedDateTime z1 = ZonedDateTime,now();
+    ZonedDateTime z2 = ZonedDateTime.of(2020, 1, 2, 3, 4, 5, 6,zoneId.of("Asia/Tokyo"));
     
+    //InstantとZonedDateTime
+    Instant i3 = Z2.toInstant();
+    ZonedDateTime z3 = i3.atZone(ZoneId.of("Europe/Lo0ndon"));
+    //ZonedTimeの利用方法
+    System.out.println("東京:" + z2.getYear() + z2.getMonth());
+    System.out.println("ロンドン:" + z3.ghetYear() + z3.getMonth() + z3.getDayofMonth());
+    if(z2.isEqual(z3)){
+      System.out.printl("これらは同じ瞬間を指しています");
+    }
   }
 }
