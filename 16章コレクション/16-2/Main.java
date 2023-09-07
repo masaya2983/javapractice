817172cb4b14c4585297226ec570fcb3d80b1086
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-07 03:04:54 +0000

    もっと便利にAPI活用術

---
+++ 16章コレクション/16-2/Main.java
@@ -0,0 +1,15 @@
+import java.util,*;
+
+public class Main{
+  public static void main(String[] args){
+    ArrayList<String> name = new ArrayLiast<String>();
+    names.add("湊");
+    names.add("朝香");
+    names.add("菅原");
+    Iterator<String> it = name.iterator();
+      while (it.hasNext()){
+        String e = it.next();
+        System.out.println(e);
+      }
+  }
+}
