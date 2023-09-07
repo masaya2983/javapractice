e94fe2742dd318e801827318649ac90579bf9496
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-07 02:38:28 +0000

    Javaを支えるクラスたち

---
+++ 16章コレクション/16-1/Main.java
@@ -0,0 +1,13 @@
+import java.utilArrayLiast
+
+public class Maina{
+  public static void main(String[] args){
+    ArrayList<Integer>points = newArrayList<Integer>();
+    points.add(10);
+    points.add(80);
+    points.add(75);
+    for (int i: points){
+      System.out.pritnln(i);
+    }
+  }
+}
