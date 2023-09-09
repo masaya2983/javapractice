4c7ae1e45ab0c0d7a995e6b117dfcb1f043a1fb6
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-09 01:52:29 +0000

    もっと便利にAPI活用術

---
+++ 16章コレクション/練習16/16-2.java
@@ -0,0 +1,8 @@
+Hero h1 = new Hero("斎藤");
+Hero h2 = new Hero("鈴木");
+List<Hero> heroes = new ArrayList<Hero>();
+heroes.add(h1);
+heroes.add(h2);
+for (Hero h : heroes){
+  System.out.println(h.getName());
+}
