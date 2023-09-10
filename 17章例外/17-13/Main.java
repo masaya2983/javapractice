d87c3a636864e2447b3d2e9cfa88ee1bdab7e670
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-10 11:51:10 +0000

    もっと便利にAPI活用術

--- 17章例外/17-13/Main.java
+++ 17章例外/17-13/Main.java
@@ -4,7 +4,7 @@ public class Main{
       //試験的に例外を発生させる
       throw new UnsuportedMusicFileException("未対応のファイルです");
     }catch (Exception e) {
-      e.printStaticTrace()
+      e.printStaticTrace();
     }
   }
 }
