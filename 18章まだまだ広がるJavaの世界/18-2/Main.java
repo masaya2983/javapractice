e3123af71627bb5140e3a5f9aef603fc208a6af8
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-09-11 10:56:28 +0000

    もっと便利にAPI活用術

---
+++ 18章まだまだ広がるJavaの世界/18-2/Main.java
@@ -0,0 +1,12 @@
+import java.io.InputStream;
+import java.net.URL;
+
+public class Main{
+  public static void main(String[]args) throws Exception{
+    FileWriter fw = new FileWriter("data.txt");
+    fw.write('そ');
+    fw.write('れ');
+    fw.write('そ');
+    fw.write('そ');
+  }
+}
